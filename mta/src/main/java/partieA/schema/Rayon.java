180971597d924cce39381e30a257a6ffa7e5275b
aodren35 <aodren.letellier@etudiant.univ-rennes1.fr>
2016-12-04 01:28:02 +0100

    DEBUG rayon

--- mta/src/main/java/partieA/schema/Rayon.java
+++ mta/src/main/java/partieA/schema/Rayon.java
@@ -3,8 +3,9 @@ package partieA.schema;
 import partieA.intervenants.Client;
 
 /**
- * Classe Rayon représentant un rayon d'un seul produit
- * Objet partagé entre les threads clients et chef de rayon
+ * Classe Rayon représentant un rayon d'un seul produit Objet partagé entre les
+ * threads clients et chef de rayon
+ * 
  * @author Aodren Letellier - Jordan Monfort
  *
  */
@@ -31,14 +32,16 @@ public class Rayon {
 		this.stockArticle = stockActuel;
 	}
 
-	
 	/**
-	 * Méthode atomique permettant au thread client de prendre un article dans le rayon
-	 * @param client Le client voulant prendre un article dans le rayon
+	 * Méthode atomique permettant au thread client de prendre un article dans
+	 * le rayon
+	 * 
+	 * @param client
+	 *            Le client voulant prendre un article dans le rayon
 	 * @throws InterruptedException
 	 */
 	public synchronized void prendreArticle(Client client) throws InterruptedException {
-		//Tant que le stock est vide le client attend
+		// Tant que le stock est vide le client attend
 		while (stockArticle <= 0) {
 			client.setEtat(Client.attenteProduit);
 			System.out.println("Client " + client.getIdClient() + " : Attente produit, stock " + article + " : " + stockArticle);
@@ -52,18 +55,24 @@ public class Rayon {
 
 	/**
 	 * Méthode atomique permettant au chef de rayon de remplir un rayon
-	 * @param stockChef Stock personnel du chef de rayon correspondant au bon produit
+	 * 
+	 * @param stockChef
+	 *            Stock personnel du chef de rayon correspondant au bon produit
 	 * @return
 	 */
 	public synchronized int remplirStock(int stockChef) {
-		//Tant que le stock n'est pas plein ou que le stock perso du chef n'est pas vide
+		// Tant que le stock n'est pas plein ou que le stock perso du chef n'est
+		// pas vide
 		// Il rempli le rayon
-		while (stockArticle < Supermarche.RAYON_STOCK_MAX || stockChef >= 0) {
-			stockArticle++;
-			stockChef--;
-			// Reveil des clients qui attendaient le produit
-			notifyAll();
+		while (stockArticle < Supermarche.RAYON_STOCK_MAX) {
+			if(stockChef>0){
+				stockArticle++;
+				stockChef--;
+
+			}
 		}
+		// Reveil des clients qui attendaient le produit
+		notifyAll();
 		return stockChef;
 	}
 
